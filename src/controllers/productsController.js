74239f1d1b9cd6ab9cc3183679e16b9575829b40
DávalosLeonardo <davaloslm@hotmail.com.ar>
2021-10-01 23:39:55 -0300

    Completa rutas y controladores

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -9,27 +9,27 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const controller = {
 	// Root - Show all products
 	index: (req, res) => {
-		// Do the magic
+		res.render("products")
 	},
 
 	// Detail - Detail from one product
 	detail: (req, res) => {
-		// Do the magic
+		res.render("detail")
 	},
 
 	// Create - Form to create
 	create: (req, res) => {
-		// Do the magic
+		res.render("product-create-form")
 	},
 	
 	// Create -  Method to store
 	store: (req, res) => {
-		// Do the magic
+		res.render("product-create-form")
 	},
 
 	// Update - Form to edit
 	edit: (req, res) => {
-		// Do the magic
+		res.render("product-edit-form")
 	},
 	// Update - Method to update
 	update: (req, res) => {
